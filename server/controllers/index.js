5c0c8c70f58adc82379e9d9809a8e84db285d904
vcipriani <vince.cipriani@gmail.com>
2016-02-05 17:32:43 -0800

    begin implementation of request handling

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,15 +1,33 @@
 var models = require('../models');
+var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      res.end('the end (users get)');      
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      res.end('the end (messages)');
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      res.end('the end (users get)');      
+    },
+    post: function (req, res) {
+      // open connection
+      db.connect();
+      // if user is not already in the database (query the db to find this out)
+        // insert req.body into the database
+          // req.end(query results)
+      // else
+        // req.end("thats already in here man!")
+      // close connection
+      res.end('the end (users)');
+      db.end();
+    }
   }
 };
 
