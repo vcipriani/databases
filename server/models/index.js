0636873565349a1af930e64bb61feb568610fd96
vcipriani <vince.cipriani@gmail.com>
2016-02-06 10:47:10 -0800

    Refactor model to have separate find user function

--- server/models/index.js
+++ server/models/index.js
@@ -2,42 +2,61 @@ var db = require('../db');
 var Promise = require('bluebird');
 var _ = require('underscore');
 
-// db.connect(); // done in /db/index.js
+var checkErr = function(err, callback) {
+  if (err) {
+    callback(err, null);
+  }
+  return err;
+};
 
-module.exports = {
-  messages: {
-    get: function() {}, // a function which produces all the messages
-    post: function(message, callback) {
-      var insertQueryString = 'INSERT INTO messages (text, id_user, id_room) VALUES (?,?,?);';
-      var queryArgs = [message.message, 6, 1];
-      console.log(insertQueryString);
-      db.query(insertQueryString, queryArgs, function(err, results) {
-        if (err) {
-          callback(err, null);
-        }
-        callback(null, 'message inserted!');
-      });
-    }
+var messages = {
+  get: function() {}, // a function which produces all the messages
+  post: function(message, callback) {
+
+    var insertQueryString = 'INSERT INTO messages (text, id_user, id_room) VALUES (?,?,?);';
+    var queryArgs = [message.message, 1, 1];
+    console.log(insertQueryString);
+    db.query(insertQueryString, queryArgs, function(err, results) {
+      checkErr(err, callback);
+      callback(null, 'message inserted!');
+    });
+  }
+};
+
+var users = {
+  get: function() {},
+  post: function(userName, callback) {
+    var insertQueryString = 'INSERT INTO users (name) VALUES (?);';
+    users.getUserId(userName, function(err, results) {
+      checkErr(err, callback);
+      if (results) {
+        callback(null, 'thats already in here man!');
+      } else {
+        db.query(insertQueryString, [userName], function(err, results) {
+          if (err) {
+            callback(err, null);
+          }
+          callback(null, 'user added to db.');
+        });
+      }
+    });
   },
 
-  users: {
-    get: function() {},
-    post: function(userName, callback) {
-      var queryString = 'SELECT name FROM users WHERE name = ?;';
-      var insertQueryString = 'INSERT INTO users (name) VALUES (?);';
-      var queryArgs = [userName];
-      db.query(queryString, queryArgs, function(err, results) {
-        if (err) {
-          callback(err, null);
-        }
-        if (results.length > 0) {
-          callback(null, 'thats already in here man!');
-        } else {
-          db.query(insertQueryString, queryArgs, function(err, results) {
-            callback(null, 'user added to db.');
-          });
-        }
-      });
-    }
+  getUserId: function(userName, callback) {
+    var queryString = 'SELECT id FROM users WHERE name = ?;';
+    db.query(queryString, [userName], function(err, results) {
+      checkErr(err, callback)        
+      if (results.length > 0) {
+        callback(null, results[0].id);
+      } else {
+        callback(null, null);
+      }
+    });
   }
+};
+
+module.exports.messages = messages;
+module.exports.users = {
+  get: users.get,
+  post: users.post
 };
--- server/spec/userTest.js
+++ server/spec/userTest.js
@@ -1,5 +1,5 @@
 var models = require('../models');
 
-models.users.post('Prince Ali', function(err, result){
+models.users.post('ValVince', function(err, result) {
   console.log(err, result);
 })
